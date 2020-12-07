8aaef32f9eb92625d1c7d15390aa66b658b44081
Himanshu <rathihimanshu@live.com>
2020-12-07 23:32:36 +0530

    Update BrowserUtil.java

--- src/main/java/com/utils/BrowserUtil.java
+++ src/main/java/com/utils/BrowserUtil.java
@@ -25,11 +25,11 @@ public class BrowserUtil {
 
 			ChromeOptions options = new ChromeOptions();
 		// 	below line of code to run your tests in headless mode
-			options.add_argument('--ignore-certificate-errors');
-			 options.addArguments("headless");
+		//	options.add_argument('--ignore-certificate-errors');
+		//	 options.addArguments("headless");
 			
-			capabilities = options.to_capabilities()
-			capabilities['acceptInsecureCerts'] = True
+		//	capabilities = options.to_capabilities()
+		//	capabilities['acceptInsecureCerts'] = True
 
 			 
 			WebDriverManager.chromedriver().setup();
